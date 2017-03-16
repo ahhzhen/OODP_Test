1ce84e7748e9c8e98be1baa901ebdc185dfde20b
ahhzhen <ahhzhen@gmail.com>
2017-03-16 17:05:49 +0800

    Removing redundant codes

--- Desktop/OODP_GIT/Assignment/src/Student.java
+++ Desktop/OODP_GIT/Assignment/src/Student.java
@@ -41,34 +41,4 @@ public class Student extends User {
 		System.out.println("7. Quit");
 	}
 	
-/*	public void startSession()
-	{
-		int choice = 0;
-		Scanner input = new Scanner(System.in);
-		while(choice != 0)
-		{
-			displayMenu();
-			System.out.println("Please select choice from menu: ");
-			try{
-			choice = input.nextInt();}
-			catch(Exception e){}
-		switch(choice)
-		{
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		case 7:
-			break;
-			default:System.out.println("Wrong input detected, please try again!");
-		}*/
-	
 }
