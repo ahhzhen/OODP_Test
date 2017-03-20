06f00a19f12b7e8795de7daa0e9d9725ea283910
User <User@10.27.82.42>
2017-03-20 20:39:51 +0800

    courses

--- Desktop/OODP_GIT/Assignment/src/Course.java
+++ Desktop/OODP_GIT/Assignment/src/Course.java
@@ -1,10 +1,22 @@
 import java.util.*;
-public class Course 
-{
-	//test???
+
+public class Course {
+	// test???
 	private String name;
 	private String courseCode;
 	private String school;
 	private ArrayList<CourseIndex> cIndexList;
-	
+
+	public Course(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public Course() {
+
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
 }
