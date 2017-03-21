2685730f9eff9e865cb09af0d88a0641ccb76e8e
hongsum <hongsum1994@hotmail.com>
2017-03-22 00:23:41 +0800

    Course update 2

--- Desktop/OODP_GIT/Assignment/src/Course.java
+++ Desktop/OODP_GIT/Assignment/src/Course.java
@@ -30,14 +30,26 @@ public class Course {
 		cindex=courseindex;
 		return cindex;
 	}
-	public void UpdateIndex(int cindex, int nindex)
+	public void UpdateIndex(int cIndex, int nIndex)
 	{
+		for(int i=0; i<cIndexList.size(); i++)
+		{
+			if(Integer.parseInt(cIndexList.get(i).toString())==cIndex)
+			{
+				System.out.println("Index exist");
+			}
+			else
+			{
+				cIndexList.get(i).UpdateIndex(nIndex);
+				cIndexList.get(i).UpdateVacancy(nIndex);
+			}
+		}
 		//check arraylist for matching index'
 		//switch with new index
 	}
 	public void UpdateVacancy(int cindex, int nindex)
 	{
-		
+	
 	}
 	
 }
--- Desktop/OODP_GIT/Assignment/src/CourseIndex.java
+++ Desktop/OODP_GIT/Assignment/src/CourseIndex.java
@@ -8,7 +8,7 @@
 		return vacancy;
 	}
 
-	private ArrayList<timeSlot> lessons = new ArrayList<timeSlot>();
+	//private ArrayList<timeSlot> lessons = new ArrayList<timeSlot>();
   	public CourseIndex(int in, int vac)
   	{
   		vacancy = vac;
@@ -20,11 +20,18 @@
   		vacancy=vac;
   		index=in;
   	}
-  	public int getIndex()
+  	public CourseIndex() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getIndex()
   	{
   		return index;
   	}
-  	public void UpdateIndex(int index)
+  	
+  	public void setIndex(int index) {
+		this.index = index;
+	}
+	public void UpdateIndex(int index)
   	{
   		
   	}
