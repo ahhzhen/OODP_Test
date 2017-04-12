afa9b7d037e82bb7b3e7e18ca414216c4ce473ba
jeanellechek <jeanellechek@192.168.1.6>
2017-04-13 01:52:44 +0800

    Done commenting course

--- STARSApp/src/Course.java
+++ STARSApp/src/Course.java
@@ -9,14 +9,28 @@ public class Course implements Serializable {
 	private int indexPointer;
 	private ArrayList<CourseIndex> cIndexList;
 	private ArrayList<TimeSlot> lectureList;
-
+	/**
+	 * This method is the constructor for course class. It does nothing.
+	 */
 	public Course() {
 	}
-
+	/**
+	 * This method is an overloading constructor with 1 parameter for course class. 
+	 * It initalise the course code given by the user.
+	 *
+	 * @param  courseCode  the course code given by the user.
+	 */
 	public Course(String courseCode) {
 		this.courseCode = courseCode;
 	}
-
+	/**
+	 * This method is an overloading constructor with 4 parameter for course class.
+	 * It initalises name, courseCode, school and an arrayList of timeslots.
+	 * @param	name		the name of the course
+	 * @param	courseCode	the courseCode of the course
+	 * @param	school		the school of the course
+	 * @param	lect		the list of lecture,lab and tutorial timing
+	 */
 	public Course(String name, String courseCode, String school, ArrayList<TimeSlot> lect) {
 		this.name = name;
 		this.courseCode = courseCode;
@@ -24,31 +38,61 @@ public class Course implements Serializable {
 		this.lectureList = lect;
 		this.cIndexList = new ArrayList<CourseIndex>();
 	}
+	/**
+	 * This method returns the name of the course.
+	 *
+	 * @return      the name of the course
+	 */
 
 	public String getName() {
 		return name;
 	}
+	/**
+	 * This method initalise the name of the course.
+	 * @param  name		the name of the course
+	 */
 
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	/**
+	 * This method returns the course code.
+	 *
+	 * @return      the courseCode of the course
+	 */
 	public String getCourseCode() {
 		return courseCode;
 	}
-
+	/**
+	 * This method initalise the course code of the course.
+	 *
+	 * @param  courseCode  the courseCode of the course
+	 */
 	public void setCourseCode(String courseCode) {
 		this.courseCode = courseCode;
 	}
-
+	/**
+	 * This method retuns the school of the course
+	 *
+	 * @return      the school of the course
+	 */
 	public String getSchool() {
 		return school;
 	}
-
+	/**
+	 * This method initalise the school of the course.
+	 *
+	 * @param  school  the school of the course
+	 */
 	public void setSchool(String school) {
 		this.school = school;
 	}
-
+	/**
+	 * This method update the index of the course.
+	 *
+	 * @param  oldIndex		the index to be changed
+	 * @param  newIndex		the index that is to be changed to
+	 */
 	public void updateIndex(int oldIndex, int newIndex) {//  String courseCode can be 2 args
 		if (selectIndex(oldIndex)) {
 			cIndexList.get(indexPointer).setIndex(newIndex);
@@ -56,7 +100,12 @@ public class Course implements Serializable {
 		} else
 			System.out.println("Update failed.");
 	}
-
+	/**
+	 * This method updates the vacancy of the course.
+	 *
+	 * @param  index	the index of a course
+	 * @param  vacancy	the number of vacancy in a course.
+	 */
 	public void updateVacancy(int index, int vacancy) {
 		if (selectIndex(index)) {
 			CourseIndex cIndex = cIndexList.get(indexPointer);
@@ -68,11 +117,23 @@ public class Course implements Serializable {
 		} else
 			System.out.println("Update failed");
 	}
-
+	/**
+	 * This method returns an arrayList of index numbers.
+	 * @return      the arrayList of courseIndex
+	 * @see         Image
+	 */
 	public ArrayList<CourseIndex> getcIndexList() {
 		return cIndexList;
 	}
-	
+	/**
+	 * This method initalise courseIndex based on the index provided
+	 * by the user.
+	 *
+	 * @param  index	the courseIndex of the course
+	 * @return      the courseIndex of the course, if found
+	 * @return		null, if courseIndex is not found
+	 * @see         Image
+	 */
 	public CourseIndex getcIndex(int index) {
 		CourseIndex cIn = null;
 		for(int i = 0; i < cIndexList.size(); i++) {
@@ -82,7 +143,11 @@ public class Course implements Serializable {
 		}
 		return cIn;
 	}
-
+	/**
+	 * This method decrements the method vacancy of a course.
+	 *
+	 * @param  index  the index of course to be decremented.
+	 */
 	public void minusFromIndex(int index) {
 		if (selectIndex(index)) {
 			cIndexList.get(indexPointer).decrementVacancy();
@@ -90,7 +155,12 @@ public class Course implements Serializable {
 		} else
 			System.out.println("Decrement failed.");
 	}
-
+	/**
+	 * This method adds index to a course.
+	 *
+	 * @param  index  index of course to be added
+	 * @return      the course returns the course
+	 */
 	public Course addToIndex(int index) {
 		if (selectIndex(index)) {
 			CourseIndex cind = cIndexList.get(indexPointer);
@@ -107,7 +177,9 @@ public class Course implements Serializable {
 			System.out.println("Increment failed.");
 		return this;
 	}
-
+	/**
+	 * This method checks the vacancy of a course.
+	 */
 	public void checkVacancy()
 	{
 		Scanner input = new Scanner(System.in);
@@ -118,7 +190,13 @@ public class Course implements Serializable {
 		else
 			System.out.println("Index does not exist.");
 	}
-	
+	/**
+	 * This method retrieve vacancy of course.
+	 *
+	 * @param  index  the index to retrieve the number of vacancy.
+	 * @return      the number of vacancies, if index is found
+	 * @return		-1, if index is not found.
+	 */
 	public int retrieveVacancy(int index) {
 		if (selectIndex(index))
 			return cIndexList.get(indexPointer).getVacancy();
@@ -127,7 +205,13 @@ public class Course implements Serializable {
 			return -1;
 		}
 	}
-	
+	/**
+	 * This method returns the total number of vacancy of an index.
+	 *
+	 * @param  int  the index to retrieve the total number of vacancy
+	 * @return      the total number of vacancy, if index is found
+	 * @return		-1, if index is not found
+	 */
 	public int retrieveTotalVacancy(int index) {
 		if (selectIndex(index))
 			return cIndexList.get(indexPointer).getTotalVacancy();
@@ -136,10 +220,28 @@ public class Course implements Serializable {
 			return -1;
 		}
 	}
-
+	/**
+	 * This method adds a new entry to the index of course created.
+	 *
+	 * @param	index		the index of the course
+	 * @param	vacancy		the number of available vacancies
+	 * @param	tVacancy	the total number of vacancies
+	 * @param	gname		the name of the group
+	 * @param	t			the timeslot of tutorials
+	 * @param	l			the timeslot of labs
+	 */
 	public void AddtocIndexList(int index, int vacancy, int tVacancy, String gname, TimeSlot t, TimeSlot l) {
 		this.cIndexList.add(new CourseIndex(index, vacancy, tVacancy, gname, t,l));
 	}
+	/**
+	 * Sets an integer within a specific Course for use
+	 * in other methods requiring the particular index of
+	 * the course index list, cIndexList.
+	 * @param int index
+	 * @return a boolean, indicating success or failure.
+	 * This prevents a method from attempting to access beyond 
+	 * the scope of cIndexList.
+	 */
 
 	public boolean selectIndex(int index) {
 		for (int i = 0; i < cIndexList.size(); i++) {
@@ -151,7 +253,14 @@ public class Course implements Serializable {
 		System.out.println("Index not found.");
 		return false;
 	}
-
+	/**
+	 * Checks if the course exists with the specified coursecode.
+	 * The method checks against the list produced by getCourseList()
+	 * and returns true if the list contains a Course with the matching course code.
+	 * The method will return false if otherwise.
+	 * @param String courseCode
+	 * @return boolean
+	 */
 	public static boolean courseExist(String courseCode) {
 		List list = getCourseList();
 
@@ -170,7 +279,13 @@ public class Course implements Serializable {
 		}
 		return breakout;
 	}
-
+	/**
+	 * This method returns the course.
+	 *
+	 * @param  courseCode  the course code of the course
+	 * @return      the course, if course code is found
+	 * @return		null, if course code is not found
+	 */
 	public static Course getCourse(String coursecode) {
 		Course c = null;
 		List courselist = getCourseList();
@@ -181,7 +296,11 @@ public class Course implements Serializable {
 		}
 		return c;
 	}
-	
+	/**
+	 * This method adds a new course.
+	 *
+	 * @param  courseCode  the course code of the course
+	 */
 	public static void addCourse(String coursecode) {
 		Scanner input = new Scanner(System.in);
 		Course course = new Course();
@@ -229,7 +348,9 @@ public class Course implements Serializable {
 			System.out.println("Error occured. Course is not added.");
 		}
 	}
-	
+	/**
+	 * This method adds a new index to the course created.
+	 */
 	public void addNewIndex()
 	{
 		Scanner input = new Scanner(System.in);
@@ -257,7 +378,13 @@ public class Course implements Serializable {
 			AddtocIndexList(indexNo, vacancy, vacancy, groupName, tut, lab);
 		}
 	}
-	
+	/**
+	 * This method creates a new time slot which allows the user to enter
+	 * and choose the day based on the classType.
+	 *
+	 * @param  classType  the type of the class.
+	 * @return      the timeSlot
+	 */
 	public static TimeSlot createTimeSlot(String classType)
 	{
 		Scanner input = new Scanner(System.in);
@@ -290,6 +417,11 @@ public class Course implements Serializable {
 			return ts;
 		}
 	}
+	/**
+	 * This method edits the name of the course.
+	 *
+	 * @param  courseName	the name of the course
+	 */
 
 	public void editCourseName(String coursename) {
 		try {
@@ -300,6 +432,11 @@ public class Course implements Serializable {
 		}
 	}
 
+	/**
+	 * This method edit the school which the course belongs to. 
+	 *
+	 * @param  school  the school  of which the course belongs to.
+	 */
 	public void editSchool(String school) {
 		try {
 			setSchool(school);
@@ -308,6 +445,12 @@ public class Course implements Serializable {
 			System.out.println("Error occured. School of course is not changed.");
 		}
 	}
+	/**
+	 * This method edits the index of the course. 
+	 *
+	 * @param  oldIndex		the index to be changed
+	 * @param  newIndex		the index to be changed to
+	 */
 	
 	public void editIndex(int oldIndex, int newIndex) {
 		try{
@@ -324,6 +467,14 @@ public class Course implements Serializable {
 			System.out.println("Error occured. ");
 		}
 	}
+	
+	/**
+	 * This method checks if a index number of a course exist. 
+	 *
+	 * @param  index  the index to be checked
+	 * @return      true, if course is found
+	 * @reutrn		false, if course is not found
+	 */
 
 	public boolean indexExist(int index) {
 		if(cIndexList.size()>0) {
@@ -336,7 +487,12 @@ public class Course implements Serializable {
 		}
 		else return false;
 	}
-	
+	/**
+	 * This method gets the timeslots of lessons. 
+	 *
+	 * @param  courseIndex  the index to be taken
+	 * @return      the list contain timeslots of the index
+	 */
 	public List getTimeSlots(int courseIndex) {
 		List<TimeSlot> list = new ArrayList();
 		for(int j = 0; j<lectureList.size(); j++) {
@@ -351,6 +507,15 @@ public class Course implements Serializable {
 		}
 		return list;
 	}
+	/**
+	 * This method checks if there are any clashes of lessons when a student register
+	 * for a course.
+	 *
+	 * @param  tsList  		the list of timeSlots of a courseIndex
+	 * @param  courseIndex the courseIndex of timeslots be return
+	 * @return      false, if there are not clashes
+	 * @return		true, if there are clashes
+	 */
 	
 	public boolean checkClash(List<TimeSlot> tsList, int courseIndex) {
 		boolean clash = false;
@@ -367,7 +532,13 @@ public class Course implements Serializable {
 		}
 		return false;
 	}
-	
+	/**
+	 * This method adds a student to the waitList if there are no vacancies
+	 * for a courseIndex.
+	 *
+	 * @param  courseIndex  the courseIndex which the students want to add
+	 * @param  matricNo the matricNo of the student
+	 */
 	public void addToWaitList(int courseIndex, String matricNo) {
 		for(int i = 0; i<cIndexList.size(); i++) {
 			CourseIndex cIndex = cIndexList.get(i);
@@ -377,7 +548,14 @@ public class Course implements Serializable {
 			}
 		}
 	}
-	
+	/**
+	 * This method is to check if a student exist in the waitlist.
+	 *
+	 * @param  courseIndex	the index which the student want to add to
+	 * @param  matricNo		the matricNo of the student
+	 * @return      true, if the student exist in the waitlist
+	 * @return		false, if the student does not exist in the waitlist.
+	 */
 	public boolean existInWaitList(int courseIndex, String matricNo)
 	{
 		for(int i = 0; i<cIndexList.size(); i++) {
@@ -387,7 +565,9 @@ public class Course implements Serializable {
 		}
 		return false;
 	}
-	
+	/**
+	 * This method prints the indexes.
+	 */
 	public void printIndexes()
 	{
 		for(int i = 0; i < cIndexList.size(); i++) {
@@ -395,12 +575,18 @@ public class Course implements Serializable {
 			System.out.println( i+1 + "\t" + cIndex.getIndex() + "\t" + cIndex.getGroupName());
 		}
 	}
-	
+	/**
+	 * This method prints a list of students.
+	 */
 	public void printStudents()
 	{
 		StudentCourse.printStudents(courseCode);
 	}
-	
+	/**
+	 * This method prints a list ordered by courseIndex.
+	 *
+	 * @param  courseIndex  the index of the course.
+	 */
 	public void printStudentsByIndex(int courseIndex)
 	{
 		if(indexExist(courseIndex)) {
@@ -409,11 +595,18 @@ public class Course implements Serializable {
 		else
 			System.out.println("Index does not exist.");
 	}
-
+	/**
+	 * This method returns the a list of courses.
+	 * @return	a list of courses
+	 */
 	public static List getCourseList() {
 		return getCourseList("courselist.dat");
 	}
-
+	/**
+	 * This method returns a list of courses. 
+	 *
+	 *@return	A list of courses
+	 */
 	public static List getCourseList(String filename) {
 		List list = null;
 		try {
@@ -424,11 +617,17 @@ public class Course implements Serializable {
 			list = new ArrayList();
 		return list;
 	}
-
+	/**
+	 * Save amendmends made to text file.
+	 *
+	 * @param  list  the list which contains the details
+	 */
 	public static void save(List list) {
 		FileIO.writeToFile("courselist.dat", list);
 	}
-	
+	/**
+	 * Save amendments made.
+	 */
 	public void save()
 	{
 		List list = getCourseList();
